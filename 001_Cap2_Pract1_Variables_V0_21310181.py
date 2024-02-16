f76412d8f552684221b390f4d0d3ad5b464bc0a6
PauloMRaz <160150088+PauloMRaz@users.noreply.github.com>
2024-02-16 08:31:24 -0600

    Add files via upload

---
+++ 001_Cap2_Pract1_Variables_V0_21310181.py
@@ -0,0 +1,11 @@
+#Paulo Enrique Muñoz Razón    21310181_E1      16/Febrero/2024
+#Inteligencia artificial
+#Capitulo 2 "Variables"
+
+mensaje="Hola mundo"
+
+a=5
+b=2      #Las lineas de codigo se leen de arriba a bajo 
+c=a+b
+
+print(c)
