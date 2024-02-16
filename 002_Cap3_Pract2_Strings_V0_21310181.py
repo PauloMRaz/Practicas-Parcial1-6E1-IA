f53d058e5b0e0a22e7989a0882f99576622c6e18
PauloMRaz <160150088+PauloMRaz@users.noreply.github.com>
2024-02-16 08:44:14 -0600

    Add files via upload

---
+++ 002_Cap3_Pract2_Strings_V0_21310181.py
@@ -0,0 +1,11 @@
+#Paulo Enrique Muñoz Razón    21310181_E1      16/Febrero/2024
+#Inteligencia artificial
+#Capitulo 3 "String"
+
+TxT1='Hola'    
+TxT2="mundo"
+
+frase='Hola "Mundo"'
+#frase="Hola'Mundo'"    #Tambien se puede poner en este orden para
+                        #que salga las comillas simples al imprimir
+print(frase)
