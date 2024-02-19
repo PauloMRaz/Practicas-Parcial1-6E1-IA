fbfdb24fbc243594384e82f191d2a43261c6ca68
PauloMRaz <160150088+PauloMRaz@users.noreply.github.com>
2024-02-19 13:10:58 -0600

    Add files via upload

---
+++ 003_Cap4_Pract3_ComoConcatenar_V0_21310181.py
@@ -0,0 +1,31 @@
+#Paulo Enrique Muñoz Razón    21310181_E1      19/Febrero/2024
+#Inteligencia artificial
+#Capitulo 4 "Como concatenar"
+
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+
+C= "Hola "+"Mundo"
+print(C)
+
+P1= "Inteligencia"
+P2= "Artificial"
+Frase= P1+" "+P2
+print(Frase)
+
+P3="Hola"
+print(P3+" Mundo")
+
+P4="Paulo "
+P5="Enrique "
+P6="Muñoz "
+P7="Razón"
+Frase1=P4+P5+P6+P7
+print(Frase1)
+
+num1='66'
+num2='77'
+suma=num1+num2
+print(suma)
---
+++ 004_Cap5_Pract4_Métodos upper(), lower() y title()_V0_21310181.py
@@ -0,0 +1,17 @@
+#Paulo Enrique Muñoz Razón    21310181_E1      19/Febrero/2024
+#Inteligencia artificial
+#Capitulo 5 "Los métodos upper(), lower() y title()"
+
+#!/usr/bin/env python
+# -*- coding: cp1252 -*-
+# -*- coding: 850 -*-
+# -*- coding: utf-8 -*-
+
+n1="paulo enrique muñoz razón"
+print(n1.title())#El title pone las primeras letras de las palabras en mayuscula
+
+n2="Esta Es Una Frase Para Ser Formateada".lower()
+print(n2)#El lower se usa para poner todo en minuscula
+
+n3="Esta Es Una Frase Para Ser Formateada"
+print(n3.upper())#El upper se usa para poner todo en mayuscula
